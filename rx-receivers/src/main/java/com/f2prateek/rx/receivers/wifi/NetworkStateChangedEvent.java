05435b25ea7d9b14984f8194ed9cae8045368675
Prateek Srivastava <f2prateek@gmail.com>
2015-09-18 20:57:08 -0700

    Use AutoValue

--- rx-receivers/src/main/java/com/f2prateek/rx/receivers/wifi/NetworkStateChangedEvent.java
+++ rx-receivers/src/main/java/com/f2prateek/rx/receivers/wifi/NetworkStateChangedEvent.java
@@ -6,61 +6,20 @@ import android.support.annotation.CheckResult;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import com.f2prateek.rx.receivers.internal.Preconditions;
+import com.google.auto.value.AutoValue;
 
-public final class NetworkStateChangedEvent {
+@AutoValue
+public abstract class NetworkStateChangedEvent {
   @CheckResult @NonNull //
   public static NetworkStateChangedEvent create(@NonNull NetworkInfo networkInfo,
       @Nullable String bssid, @Nullable WifiInfo wifiInfo) {
     Preconditions.checkNotNull(networkInfo, "networkInfo == null");
-    return new NetworkStateChangedEvent(networkInfo, bssid, wifiInfo);
+    return new AutoValue_NetworkStateChangedEvent(networkInfo, bssid, wifiInfo);
   }
 
-  private final NetworkInfo networkInfo;
-  private final String bssid;
-  private final WifiInfo wifiInfo;
+  public abstract @NonNull NetworkInfo networkInfo();
 
-  private NetworkStateChangedEvent(@NonNull NetworkInfo networkInfo, @Nullable String bssid,
-      @Nullable WifiInfo wifiInfo) {
-    this.networkInfo = networkInfo;
-    this.bssid = bssid;
-    this.wifiInfo = wifiInfo;
-  }
+  public abstract @Nullable String bssid();
 
-  public @NonNull NetworkInfo networkInfo() {
-    return networkInfo;
-  }
-
-  public @Nullable String bssid() {
-    return bssid;
-  }
-
-  public @Nullable WifiInfo wifiInfo() {
-    return wifiInfo;
-  }
-
-  @Override public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-
-    NetworkStateChangedEvent that = (NetworkStateChangedEvent) o;
-
-    if (!networkInfo.equals(that.networkInfo)) return false;
-    if (bssid != null ? !bssid.equals(that.bssid) : that.bssid != null) return false;
-    return !(wifiInfo != null ? !wifiInfo.equals(that.wifiInfo) : that.wifiInfo != null);
-  }
-
-  @Override public int hashCode() {
-    int result = networkInfo.hashCode();
-    result = 31 * result + (bssid != null ? bssid.hashCode() : 0);
-    result = 31 * result + (wifiInfo != null ? wifiInfo.hashCode() : 0);
-    return result;
-  }
-
-  @Override public String toString() {
-    return "NetworkStateChangedEvent{" +
-        "networkInfo=" + networkInfo +
-        ", bssid='" + bssid + '\'' +
-        ", wifiInfo=" + wifiInfo +
-        '}';
-  }
+  public abstract @Nullable WifiInfo wifiInfo();
 }
